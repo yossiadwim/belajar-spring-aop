4ae4a6804517e89531415b5056b0401962068da0
Yossia Dwi Mahardika <yossia.dwi@ti.ukdw.ac.id>
2025-02-22 20:40:35 +0700

    Initial commit

---
+++ src/test/java/com/example/belajar_spring_aop/BelajarSpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
